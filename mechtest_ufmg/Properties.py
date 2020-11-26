5b1612f7761dda321c838c80d5b62ede09fd026a
pedro <pdrodresende@gmail.com>
2020-11-26 11:06:02 -0300

    docstrings

--- mechtest_ufmg/Properties.py
+++ mechtest_ufmg/Properties.py
@@ -7,131 +7,211 @@ import os
 
 def plot_eng_SSC(strain, stress, fig_label = 'Sample ', show_plot = True,
                  save = False, name = 'eng_SSC', ):
-        
-        plt.figure(figsize=(8, 4.5), facecolor = 'white')
-        plt.plot(strain, stress, 'b-', label = fig_label)
-        plt.xlabel('strain [mm/mm]')
-        plt.ylabel('stress [MPa]')
-        plt.xlim(0, 1.05 * max(strain))
-        plt.ylim(0, 1.05 * max(stress))
-        plt.title(f'Engineering stress/strain curve')
-        plt.legend(fontsize = 12, loc = 'lower right', frameon = False)
 
-        if save == True:
-            save_path = os.path.abspath(os.path.join('output', name))
-            plt.savefig(save_path, dpi = 300, bbox_inches = 'tight',transparent = False)
+    '''
+    Plots the conventional, or engineering, stress-strain curves.
+    Inputs:
+    strain - vector containing the strain data in admensional units, if the data is provided in %, divide it by 100; e.g.: [mm/mm].
+    stress - vector containing the stress data relative to the strain vector.
+    fig_label - the name of the sample or test run that will appear in the legend.
+    show_plot - default = True; if set to false, the plot is not shown when the script runs.
+    save - default = False; if set to true, will save the figure ina output folder within the running directory.
+    name - default = eng_SSC; sets the name of the file that will be saved.
+    Outputs:
+    Engineering stress-strain curve as a file or image shown with matplotlib interface.
+    '''
         
-        if show_plot == True:
-            plt.show()
+    plt.figure(figsize=(8, 4.5), facecolor = 'white')
+    plt.plot(strain, stress, 'b-', label = fig_label)
+    plt.xlabel('strain [mm/mm]')
+    plt.ylabel('stress [MPa]')
+    plt.xlim(0, 1.05 * max(strain))
+    plt.ylim(0, 1.05 * max(stress))
+    plt.title(f'Engineering stress/strain curve')
+    plt.legend(fontsize = 12, loc = 'lower right', frameon = False)
+
+    if save == True:
+        save_path = os.path.abspath(os.path.join('output', name))
+        plt.savefig(save_path, dpi = 300, bbox_inches = 'tight',transparent = False)
+    
+    if show_plot == True:
+        plt.show()
 
 
 
 def young_modulus(strain, stress, fig_label = 'Sample', show_plot = True, save = False, name = 'elasticity'):
 
+    '''
+    Calculates the apparent modulus of elasticity of the material from the engineering stress-strain data. 
+    
+    Inputs:
+    strain - vector containing the strain data in admensional units, if the data is provided in %, divide it by 100; e.g.: [mm/mm].
+    stress - vector containing the stress data relative to the strain vector.
+    fig_label - the name of the sample or test run that will appear in the legend.
+    show_plot - default = True; if set to false, the plot is not shown when the script runs.
+    save - default = False; if set to true, will save the figure ina output folder within the running directory.
+    name - default = elasticity; sets the name of the file that will be saved.
+    
+    Outputs:
+    E - the apparent modulus of elasticity in the same units as the stress input; e.g.: [MPa].
+    b - the stress intercept obtained from the regression process.
+    R_squared - the R^2 statistics related to the linear regression of the linear portion of the stress-strain curve.
+
+    Figure showing the portion of the curve that was fitted and the regression data, except b.
+    '''
+
     # taking only the elastic portion of a curve
-        x = strain[strain < 0.002]
-        y = stress[0:len(x)]
+    x = strain[strain < 0.002]
+    y = stress[0:len(x)]
 
     # performing the linear regression on the elastic part of the data
-        init_guess = [100000, 0]
-        model = curve_fit(Hooke, x, y, p0 = init_guess)
-        ans, cov = model
-        E, b = ans
-        fit_curve = E * x + b
-        E_gpa = round(E / 1000)
+    init_guess = [100000, 0]
+    model = curve_fit(Hooke, x, y, p0 = init_guess)
+    ans, cov = model
+    E, b = ans
+    fit_curve = E * x + b
+    E_gpa = round(E / 1000)
 
     # calculating the R_squared statistic
-        r2 = r_squared(x, y, Hooke, ans)
+    r2 = r_squared(x, y, Hooke, ans)
         
     # plotting the figure and showing or saving the figure
-        plt.figure(figsize = (8,4.5))
-        plt.plot(strain, stress, 'b-', label = fig_label)
-        plt.plot(x, fit_curve, 'r-', linewidth = 2, label = 'Fitted curve' )
-        plt.xlabel('strain [mm/mm]')
-        plt.ylabel('stress [MPa]')
-        plt.xlim(0, 1.05* max(strain))
-        plt.ylim(0, 1.05 * max(stress))
-        plt.title(f'Elasticity modulus determination')
-        plt.legend(fontsize = 12, loc = 'lower right', frameon = False)
-        plt.text(0.1 * max(strain), 0.1 * max(stress), f'The elasticity modulus is {E_gpa} GPa, R² = {round(r2, 4)}', fontsize = 12)
+    plt.figure(figsize = (8,4.5))
+    plt.plot(strain, stress, 'b-', label = fig_label)
+    plt.plot(x, fit_curve, 'r-', linewidth = 2, label = 'Fitted curve' )
+    plt.xlabel('strain [mm/mm]')
+    plt.ylabel('stress [MPa]')
+    plt.xlim(0, 1.05* max(strain))
+    plt.ylim(0, 1.05 * max(stress))
+    plt.title(f'Elasticity modulus determination')
+    plt.legend(fontsize = 12, loc = 'lower right', frameon = False)
+    plt.text(0.1 * max(strain), 0.1 * max(stress), f'The elasticity modulus is {E_gpa} GPa, R² = {round(r2, 4)}', fontsize = 12)
 
-        if save == True:
+    if save == True:
 
-            save_path = os.path.abspath(os.path.join('output', name))
-            plt.savefig(save_path, dpi = 300, bbox_inches = 'tight',transparent = False)        
-        
-        if show_plot == True:
-            plt.show()
+        save_path = os.path.abspath(os.path.join('output', name))
+        plt.savefig(save_path, dpi = 300, bbox_inches = 'tight',transparent = False)        
+    
+    if show_plot == True:
+        plt.show()
 
-        return E, int(b), r2
+    return E, int(b), r2
 
 def sigma_y(strain, stress, E_mpa, b = 0, fig_label = 'Sample', show_plot = True, save = False, name = 'sigma_yield'):
-        # taking part of the data
-        x = strain[strain < 0.05]
-        k = x - 0.002
-        z = Hooke(k, E_mpa)
-        # finding the index of sig_y
-        i = 0
-        while stress[i] > z[i]:
-            i = i + 1
 
-        sig_y = stress[i]  
-                
-        plt.figure(figsize = (8,4.5))
-        plt.plot(strain, stress, 'b-', label = fig_label)
-        plt.plot(x, Hooke(x, E_mpa, b), 'r--', linewidth = 1, label = 'Hooke\'s law' )
-        plt.plot(x, Hooke(k, E_mpa), 'r:', linewidth = 1 )
-        plt.xlabel('strain [mm/mm]')
-        plt.ylabel('stress [MPa]')
-        plt.xlim(0, 1.05 * max(strain))
-        plt.ylim(0, 1.05 * max(stress))
-        plt.title(f'Yield strength determination')
-        plt.legend(fontsize = 12, loc = 'lower right', frameon = False)
-        plt.text(0.1 * max(strain), 0.1 * max(stress), f'The yield strength is {round(sig_y)} MPa.', fontsize = 12)
+    '''
+    Calculates the yielding stress as the stress related to a permanent deformation of 0.002.
 
-        if save == True:
+    Inputs:
+    strain - vector containing the strain data in admensional units, if the data is provided in %, divide it by 100; e.g.: [mm/mm].
+    stress - vector containing the stress data relative to the strain vector.
+    E_mpa - the modulus of elasticity in MPa (should work if the units are consistent, but not SI), whether user stated or calculated by young_modulus function.
+    b - default = 0; the intercept of the regression for the specific data, leads to a higher precision result.
+    fig_label - the name of the sample or test run that will appear in the legend.
+    show_plot - default = True; if set to false, the plot is not shown when the script runs.
+    save - default = False; if set to true, will save the figure ina output folder within the running directory.
+    name - default = sigma_yield; sets the name of the file that will be saved.
 
-            save_path = os.path.abspath(os.path.join('output', name))
-            plt.savefig(save_path, dpi = 300, bbox_inches = 'tight',transparent = False)
+    Outputs:
+    sigma_yield - the yielding stress in the same unit of stress input.
 
-        if show_plot == True:
-            plt.show()
+    Figure showing the lines used to compare and determine the yield strength.
+    '''
+    # taking part of the data
+    x = strain[strain < 0.05]
+    k = x - 0.002
+    z = Hooke(k, E_mpa)
+    # finding the index of sig_y
+    i = 0
+    while stress[i] > z[i]:
+        i = i + 1
 
-        print(f'The yield strength is {round(sig_y)} MPa.')
-        
-        return sig_y
+    sig_y = stress[i]  
+            
+    plt.figure(figsize = (8,4.5))
+    plt.plot(strain, stress, 'b-', label = fig_label)
+    plt.plot(x, Hooke(x, E_mpa, b), 'r--', linewidth = 1, label = 'Hooke\'s law' )
+    plt.plot(x, Hooke(k, E_mpa), 'r:', linewidth = 1 )
+    plt.xlabel('strain [mm/mm]')
+    plt.ylabel('stress [MPa]')
+    plt.xlim(0, 1.05 * max(strain))
+    plt.ylim(0, 1.05 * max(stress))
+    plt.title(f'Yield strength determination')
+    plt.legend(fontsize = 12, loc = 'lower right', frameon = False)
+    plt.text(0.1 * max(strain), 0.1 * max(stress), f'The yield strength is {round(sig_y)} MPa.', fontsize = 12)
+
+    if save == True:
+
+        save_path = os.path.abspath(os.path.join('output', name))
+        plt.savefig(save_path, dpi = 300, bbox_inches = 'tight',transparent = False)
+
+    if show_plot == True:
+        plt.show()
+
+    print(f'The yield strength is {round(sig_y)} MPa.')
+    
+    return sig_y
 
 def ultimate_tens_stren(strain, stress, show = True):
+
+    '''
+    Calculates the ultimate tensile stress taking the higher stress from the stress vector input.
+
+    Inputs:
+    strain - vector containing the strain data in admensional units, if the data is provided in %, divide it by 100; e.g.: [mm/mm].
+    stress - vector containing the stress data relative to the strain vector.
+    show - default = True; if true, prints the value of UTS in the command line.
+
+    Output:
+    uts - the ultimate tensile strength in the same unit of the stress input.
+
+    '''
         
-        uts = max(stress)
-        if show == True:
-            print(f'The ultimate tensile strength is {round(uts)} MPa. ')
-    
-        return uts
+    uts = max(stress)
+    if show == True:
+        print(f'The ultimate tensile strength is {round(uts)} MPa. ')
+
+    return uts
 
 def uniform_elongation(strain, stress, show = True):
+
+    '''
+    Calculates the uniforme elongation from the input data taking the strain point related to the uts value.
+
+    Inputs:
+    strain - vector containing the strain data in admensional units, if the data is provided in %, divide it by 100; e.g.: [mm/mm].
+    stress - vector containing the stress data relative to the strain vector.
+    show - default = True; if true, prints the value of UTS in the command line.
+
+    Output:
+    u_elong - the uniform elongation of the sample.
+    '''
     # transform data to numpy arrays
-        eps = strain.to_numpy()
-        sig = stress.to_numpy()
+    eps = strain.to_numpy()
+    sig = stress.to_numpy()
 
-    # finds the index of the maximum stress
-        index = np.argmax(sig)
-        u_elong = eps[index]
+# finds the index of the maximum stress
+    index = np.argmax(sig)
+    u_elong = eps[index]
 
-    # prints depending on the user's choice
-        if show == True:
-            print(f'The uniform elongation is {round(u_elong, 4)}.')
+# prints depending on the user's choice
+    if show == True:
+        print(f'The uniform elongation is {round(u_elong, 4)}.')
 
-        return u_elong
+    return u_elong
 
 def aprox_resilience(E, sig_y, show = True):
 
-        U_r = (sig_y ** 2)/(2*E)
+    '''
+    Calculates the resilience by the approximation formula U_r = sigma_yield^2/E
+    '''
 
-        if show == True:
-            print(f'The resilience calculated by the formula yield²/2E is {round(U_r,2)} MJ/m³. ')
+    U_r = (sig_y ** 2)/(2*E)
 
-        return U_r
+    if show == True:
+        print(f'The resilience calculated by the formula yield²/2E is {round(U_r,2)} MJ/m³. ')
+
+    return U_r
 
 def resilience(strain, stress, sig_y, dx = 1.0, show = True):
     
